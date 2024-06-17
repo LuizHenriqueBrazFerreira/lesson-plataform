55d4e589d671f0112f916e6c56874393677938f2
Fabio Lessa <contatofabiolessa@gmail.com>
2024-06-17 11:45:55 -0300

    Finaliza back-end dos pdfs

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,5 +3,6 @@ import userRouter from './User.routes'
 import courseRouter from './Courses.routes'
 import moduleRouter from './Modules.routes'
 import userCoursesRouter from './UserCourses.routes'
+import pdfLessonRouter from './PdfLesson.routes'
 
-export default {lessonRouter, userRouter, courseRouter, moduleRouter, userCoursesRouter}
+export default {lessonRouter, userRouter, courseRouter, moduleRouter, userCoursesRouter, pdfLessonRouter}
