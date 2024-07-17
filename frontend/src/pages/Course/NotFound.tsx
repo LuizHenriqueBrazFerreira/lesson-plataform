aff4050d7d51e90a995a7191e2c4e9b14b05fcfd
Fábio Lessa <fbdonada@gmail.com>
2024-07-17 13:18:13 -0300

    Finaliza tradução estática

--- frontend/src/pages/Course/NotFound.tsx
+++ frontend/src/pages/Course/NotFound.tsx
@@ -1,7 +1,10 @@
+import { useTranslation } from "react-i18next";
+
 function NotFound() {
+  const { t } = useTranslation();
   return (
     <div className="w-screen h-[85vh] flex justify-center items-center">
-      404 - Página não encontrada
+      404 - {t("Página não encontrada")}
     </div>
   );
 }
