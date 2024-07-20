b666c461d4ca59ea06c6dc86dd5bc1f79076ac14
Sabino Lopes <sabinolopes007@gmail.com>
2024-07-20 14:28:36 -0300

    correção de db

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import app from './app'
 
-const PORT = process.env.API_PORT || '3001';
+const PORT = process.env.PORT || '3001';
 
 const server = app.listen(PORT, () =>  console.log(`Server is running on port: ${PORT}`))
 
