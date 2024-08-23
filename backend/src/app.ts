3123755fbf22ab3f6169d80321478c85de4def56
Sabino Lopes <sabinolopes007@gmail.com>
2024-08-22 23:40:34 -0300

    teste 14

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,14 +6,14 @@ const app = express()
 
 app.use(express.json())
 app.use(cors());
-app.use(routes.lessonRouter)
-app.use(routes.userRouter)
-app.use(routes.courseRouter)
-app.use(routes.moduleRouter)
-app.use(routes.userCoursesRouter)
-app.use(routes.pdfLessonRouter)
-app.use(routes.watchedLessonsRouter)
-app.use(routes.modulesProgressRouter)
-app.use(routes.searchBarRouter)
+app.use('/api',routes.lessonRouter)
+app.use('/api', routes.userRouter)
+app.use('/api', routes.courseRouter)
+app.use('/api', routes.moduleRouter)
+app.use('/api', routes.userCoursesRouter)
+app.use('/api', routes.pdfLessonRouter)
+app.use('/api', routes.watchedLessonsRouter)
+app.use('/api', routes.modulesProgressRouter)
+app.use('/api', routes.searchBarRouter)
 
 export default app;
